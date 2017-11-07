207bc3347e310f3bec7fe7c215d7eff2e31c13f8
Lianxu <lianxu@LianxudeMacBook-Pro.local>
2017-11-07 14:20:32 +0800

    08

--- 08-find-max.rb
+++ 08-find-max.rb
@@ -2,13 +2,23 @@
 
 print "请输入一个数字x，然后按 Enter: "
 x = gets
+x = x.to_i
 
 print "请输入一个数字y，然后按 Enter: "
 y = gets
+y = y.to_i
 
 print "请输入一个数字z，然后按 Enter: "
 z = gets
+z = z.to_i
 
 # ....
+if x > y && x > z
+  l = "x"
+elsif y > x && y > z
+  l = "y"
+else
+  l = "z"
+end
 
-puts "最大的数是 ________(x或y或z)"
+puts "最大的数是 ____#{l}____(x或y或z)"
