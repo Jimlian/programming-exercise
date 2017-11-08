d5f187c7def9b98200f3b561c196eb9415bf6c78
Lianxu <lianxu@LianxudeMacBook-Pro.local>
2017-11-08 10:01:55 +0800

    13

--- 13-nn.rb
+++ 13-nn.rb
@@ -1,11 +1,15 @@
 # 题目: 输入一个数字 N，输出 N * N 乘法表
 
 print "请输入数字 N，然后按 Enter: "
-n = gets
+n = gets.to_i
 
-# while ( ... )
-#   while ( ...)
-#
-#   end
-# end
+b  =1
+ while ( b <= n )
 
+   h =1
+   while ( h <= n )
+  puts "#{b} * #{h} = #{b*h}"
+  h += 1
+   end
+   b +=1
+ end
